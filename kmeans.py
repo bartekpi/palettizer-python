5dac8c2704e445fb8846a2e565837e1d17c111d9
Bartosz <bartosz.pieniak@gmail.com>
2020-03-01 14:17:37 +0000

    refactor so the tests dont complain about ignored warnings

--- kmeans.py
+++ kmeans.py
@@ -3,12 +3,13 @@ import numpy as np
 
 
 class KMeans():
+    np.seterr(divide='ignore', invalid='ignore')
 
     def __init__(self, n_clusters=2, max_iter=200, random_state=None, tol=1e-4):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.random_state = random_state
-        self.tol = 1e-4
+        self.tol = tol
         np.random.seed(self.random_state)
 
     def fit(self, X):
@@ -19,20 +20,18 @@ class KMeans():
         value_range = X.min()//2, X.max()//2
         for i in range(self.n_clusters):
             self.centroids[i, :] = np.random.uniform(
-                *value_range, n_dims).tolist()
+                *value_range, n_dims)
 
         for k in range(self.max_iter):
             clusters = self.predict(X)
 
-            new_centroids = np.array([X[clusters == x].mean(axis=0).tolist()
-                                      for x in range(self.n_clusters)])
-            if np.isnan(new_centroids).any():
-                new_centroids[np.isnan(new_centroids)] = (
-                    np.random.uniform(
-                        *value_range, new_centroids[
-                            np.isnan(new_centroids)].shape[0]))
-                self.centroids = deepcopy(new_centroids)
-                continue
+            new_centroids = np.random.uniform(
+                *value_range, (self.n_clusters, n_dims))
+
+            for i in range(self.n_clusters):
+                X_slice = X[clusters == i]
+                if X_slice.shape[0] > 0:
+                    new_centroids[i] = np.nanmean(X_slice, axis=0)
 
             last_distance = np.linalg.norm(new_centroids - self.centroids, axis=1).sum()
             self.centroids = deepcopy(new_centroids)
